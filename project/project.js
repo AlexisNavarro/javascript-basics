d8e1d7303409a8149b61fb04fbc6f7e1ad5b9410
Alexis Navarro <navarroalexis007@gmail.com>
2023-08-15 21:01:49 -0600

    implemented to spin the slot machine

--- project/project.js
+++ project/project.js
@@ -1,6 +1,6 @@
-//Deposit money
-//Determine number of lines to bet on
-//collect a bet amount
+//Deposit money (done)
+//Determine number of lines to bet on (done)
+//collect a bet amount (done)
 //spin the slot machine
 //check if the user won
 //give the user their winnings
@@ -8,6 +8,26 @@
 
 //import the packaged installed using require
 const prompt = require("prompt-sync")();
+
+//Creating the slot machine
+const ROWS = 3;
+const COLS = 3;
+
+//Object
+const SYMBOLS_COUNT = {
+    A: 2,
+    B: 4,
+    C: 6,
+    D: 8
+}
+
+//multiplier of each symbol
+const SYMBOLS_VALUES ={
+    A: 5,
+    B: 4,
+    C: 3,
+    D: 2
+}
 
 //first way to create a function 
 const deposit = () => {
@@ -52,6 +72,33 @@ const getBet = (balance, lines) => {
     }//end while loop
 };
 
+const spin = () =>{
+    const symbols = []; //will be adding elements, not changing values
+
+    //to loop an object, use object.entries
+    for (const [symbol, count] of Object.entries(SYMBOLS_COUNT)) {
+        for(let i = 0; i<count; i++){
+            symbols.push(symbol); //insert a new element into the array
+        }
+    }//end first for
+
+    const reels = [[],[],[]];
+
+    for(let i = 0; i < COLS; i++){
+        const reelSymbols = [...symbols]; //copy each available symbol into a reel
+        for(let j = 0; j < ROWS; j++){
+            const randomIndex = Math.floor(Math.random() * reelSymbols.length);
+            const selectedSymbol = reelSymbols[randomIndex];
+            reels[i].push(selectedSymbol);
+            reelSymbols.splice(randomIndex, 1); //remove a symbol so its not selected again
+        }
+    }
+    
+    return reels;
+};
+
+const reels = spin();
+console.log(reels);
 let balance = deposit(); // let allows us to change the value of a variable
 const numLines = getNumLines(); //const doesn't allow us to change the value of the variable once its set
 const bet = getBet(balance, numLines);
